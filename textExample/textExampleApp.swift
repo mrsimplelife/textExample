621f281c87111d1759288cf7343ec5e0cebc7448
youme <parkddongbird@gmail.com>
2023-02-12 20:12:46 +0900

    Initial Commit

---
+++ textExample/textExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  textExampleApp.swift
+//  textExample
+//
+//  Created by youme on 2023/02/12.
+//
+
+import SwiftUI
+
+@main
+struct textExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
